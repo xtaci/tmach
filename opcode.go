9418d804a4466fc8e0854fa2983e98cef1242761
xtaci <daniel820313@gmail.com>
2016-09-10 23:39:54 +0800

    upd

--- opcode.go
+++ opcode.go
@@ -1,22 +1,29 @@
-package main
+package tmach
 
 const (
-	INBOX = iota
-	OUTBOX
-	COPYFROM
-	COPYTO
-	ADD
-	SUB
-	INC
-	DEC
-	JMP
-	JZ
-	JLZ
-	// INDIRECT ACCESS
-	MCOPYFROM
-	MCOPYTO
-	MADD
-	MSUB
-	MINC
-	MDEC
+	REG0 = iota
+	REG1
+	REG2
+	REG4
+	REG5
+	REG6
+	REG7
+	REG8
+)
+
+const (
+	IN = iota
+	OUT
+	LOAD // LOAD [REGn], REGm
+	STOR // STOR REGn, [REGm]
+	ADD  // ADD REGn, REGm
+	SUB  // SUB REGn, REGm
+	MUL  // MUL REGn, REGm
+	DIV  // DIV REGn, REGm
+	INC  // INC REGn
+	DEC  // DEC REGn
+	JMP  // JMP REGn
+	JZ   // JZ REGn
+	JGZ  // JGZ REGn
+	JLZ  // JLZ REGn
 )
