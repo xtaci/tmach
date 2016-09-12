98429e12a076fce1a64b061f63f2c3b03ae27a51
xtaci <daniel820313@gmail.com>
2016-09-12 17:46:36 +0800

    upd

--- vm.go
+++ vm.go
@@ -22,28 +22,30 @@ func (p *Program) Load(code []byte) {
 }
 
 func (p *Program) run() {
-	pc := p.reg[PC]
-	for p.code[pc] != HLT {
-		opcode := p.code[pc]
-		switch opcode {
-		case IN:
-			n := p.code[pc+1]
-			p.reg[n] = <-p.in
-			pc += 2
-		case OUT:
-			n := p.code[pc+1]
-			p.in <- p.reg[n]
-			pc += 2
-		case LD:
-			n := p.code[pc+1]
-			m := p.code[pc+2]
-			p.reg[m] = p.data[n]
-			pc += 3
-		case ST:
-			n := p.code[pc+1]
-			m := p.code[pc+2]
-			p.data[m] = p.reg[n]
-			pc += 3
+	for {
+		pc := p.reg[PC]
+		if pc < int32(len(p.code)) && p.code[pc] != HLT {
+			opcode := p.code[pc]
+			switch opcode {
+			case IN:
+				n := p.code[pc+1]
+				p.reg[n] = <-p.in
+				pc += 2
+			case OUT:
+				n := p.code[pc+1]
+				p.in <- p.reg[n]
+				pc += 2
+			case LD:
+				n := p.code[pc+1]
+				m := p.code[pc+2]
+				p.reg[m] = p.data[n]
+				pc += 3
+			case ST:
+				n := p.code[pc+1]
+				m := p.code[pc+2]
+				p.data[m] = p.reg[n]
+				pc += 3
+			}
 		}
 	}
 }
